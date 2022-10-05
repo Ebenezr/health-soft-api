b610e806ef1336bfc6eeeff2ae5b1f384b1b8671
Ebenezr <ebenezarbukosia@gmail.com>
2022-10-05 16:41:48 +0300

    [FIX] minor fixes

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,8 @@ module HealthSoftApi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+ 
+    require 'sprockets/railtie'
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
